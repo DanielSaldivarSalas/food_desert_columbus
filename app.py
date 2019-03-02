5efb0393c047251c7f3af9e0d14bf04425f6588d
Josh <joshua.m.schraivogel@tsgforce.com>
2019-03-02 15:22:37 -0500

    HTML set up

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/result')
+def result():
+    mapper = {'phy':50,'che':60,'maths':70}
+    return render_template('result.html', result = mapper)
 
 if __name__ == "__main__": 
-    app.run()
+    app.run(debug = True)
